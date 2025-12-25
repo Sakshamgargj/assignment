f2510342ab90e5d009ad2177d199416e81a4bede
Sakshamgargj <garganmol154@gmail.com>
2025-12-25 23:12:58 +0530

    backend

---
+++ backend/src/App.js
@@ -0,0 +1,47 @@
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import express from 'express'
+import articleRoutes from "./routes/article-routes.js";
+
+const app = express()
+
+const allowedOrigins = [
+  "http://localhost:5173"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+  res.status(200).json({ status: 'OK', message: 'Server is running!!' });
+})
+
+app.use("/api/articles", articleRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: 'Something went wrong!',
+    error: err.message
+  });
+});
+
+export { app }
---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import {app} from './App.js'
+import connectDB from "./DB/blog-db.js";
+
+dotenv.config();
+console.log("PORT: ",process.env.PORT)
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+            console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGODB connection failed !!! ", err);
+    })
